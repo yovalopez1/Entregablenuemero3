4595de2378531b2689aee841a60d518ec0f835c7
JOVANY ALBEIRO LOPEZ ZAPATA <jovany_lopez82101@elpoli.edu.co>
2021-11-18 11:56:34 -0500

    proyecto entregable numero 3V.1

--- Point1DOM.js
+++ Point1DOM.js
@@ -1,46 +1,16 @@
-/*const ex1button = document.querySelector('#ex1button')
-if(ex1button){
-    ex1button.addEventListener('click' , function(event) {
-        const ex1subjects = document.querySelector('#ex1subjects')
-        const ex1price = document.querySelector('#ex1price')
-        const ex1name = document.querySelector('#ex1name')
-        const ex1result = document.querySelector('#ex1result')
-
-        if(ex1subjects && ex1price && ex1name && ex1result){
-            let nameValue = ex1name.value
-            let subjectsValue = Number(ex1subjects.value)
-            let priceValue = Number(ex1price.value)
-            var msg = ""
-
-            if(nameValue == '' || subjectsValue == '' || priceValue == ''){
-                msg = 'Please fill fields'
-            }else{
-                let total = calculate(subjectsValue, priceValue)
-                msg = 'Hi ' + nameValue + '. Please to pay ' + total
-                cleanFields()  
-            } 
-            ex1result.textContent = msg
-        }else{
-            alert('Error')
-        }
-    })
-}*/
-
 const button = document.querySelector('#button')
 if(button){
     button.addEventListener('click' , function(event) {
-        const pasajero = document.querySelector('#pasajero')
+        const pasajero = document.querySelector('#input_Pasajero')
         const Ruta = document.querySelector('#input_ruta')
-        const result = document.querySelector('#result')
         console.log(Ruta)
         console.log(pasajero)
-        console.log(result)
-        if(Ruta && pasajero && result){
+        if(Ruta && pasajero){
             let pasajeroValue = pasajero.value
             let RutaValue = Ruta.value
             var msg = ""
             console.log("aqui")
-            if(nameValue == '' || RutaValue == ''){
+            if(pasajeroValue == '' || RutaValue == ''){
                 msg = 'Por fovor llene los campos'
             }else{
                 console.log("aqui1")
@@ -51,7 +21,6 @@ if(button){
                     total = pasajeroValue*1000
                     alert("El dinero recolectado es: " + total)
                 } 
-                //result.textContent = msg
             } 
         }else{
             alert('Error')
